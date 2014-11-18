78cb3e9f9b6cdc158fcff2456c90cb09e18f8611
Joseph Dykstra <josephdykstra@gmail.com>
2014-11-18 16:41:03 -0600

    Yay; it works

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-var trello = require('./trello.json')
-var util = require('util')
-var obj = trello.cards.reduce(function (memo, curr) {
-	memo[curr.idList] = memo[curr.idList] || []
-	memo[curr.idList].push(curr.name)
-	return memo
-}, {})
-console.dir(obj)
+var https = require('https')
+var xtend = require('xtend')
+var url = require('url')
+var defaultUrl = require('./defaultUrl.json')
+
+function parseArgs(arg) {
+	arg = arg || process.argv[2]
+	var result = {}
+	result = /[\.\/:]/.test(arg)?
+		url.parse(arg) :
+		{pathname: '/b/' + arg + '.json'}
+	result = xtend(defaultUrl, result)
+	return url.format(result)
+}
+
+function get(trelloUrl, cb) {
+	var req = https.request(trelloUrl, function (res) {
+		var str = ''
+		res.on('error', console.log.bind(console, 'res err:'))
+		res.on('data', function (c) { str += c.toString() }) //''.concat.bind(str))
+		res.on('end', function () { cb(str) })
+	})
+	req.on('error', console.log.bind(console, 'req err:'))
+	req.end()
+}
+
+function parseJson(trelloJson, showClosed) {
+	var parsed = JSON.parse(trelloJson)
+	/*require('fs').writeFileSync(
+		'trello' + Math.random().toString().slice(2) + '.txt',
+		require('util').inspect(parsed.lists, {depth:1}),
+		{encoding:'utf8'}
+	)*/
+	var idToName = parsed.lists.reduce(function (memo, curr) {
+		if (showClosed || !curr.closed) {
+			memo[curr.id] = curr.name
+		}
+		return memo
+	}, {})
+	var applied = parsed.cards.reduce(function (memo, curr) {
+		var name = idToName[curr.idList]
+		if (showClosed || (name && !curr.closed)) {
+			memo[name] = memo[name] || []
+			memo[name].push(curr.name)
+		}
+		return memo
+	}, {})
+	return applied
+}
+
+(function () {
+	var trelloUrl = parseArgs()
+	console.log(trelloUrl)
+	get(trelloUrl, function (trelloJson) {
+		console.dir(parseJson(trelloJson, false))
+	})
+})()
---
+++ readme.md
@@ -0,0 +1,25 @@
+trello-json-parser
+==================
+
+#Usage
+
+```
+$ npm install -g trello-json-parser
+$ trello-json-parser 6xbOmILH
+https://trello.com/b/6xbOmILH.json
+{
+  "my first list": [
+    "a card",
+    "another card"
+  ],
+  "another list": [
+    "moar cards",
+    "cards,",
+    "Cards,",
+    "CARDS!!!"
+  ]
+}
+```
+
+#License
+[VOL](http://veryopenlicense.com)
